ec97a94d7820b25928e98e65b97da4e4bfb38bcb
Моравец Влад <V-moravec@mail.ru>
2020-01-20 21:04:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from cell_class import Cell
+import sqlite3
+import re
 import db_working
 from telegram import Bot
 from telegram import Update
@@ -16,9 +18,10 @@ def do_start(bot: Bot, update: Update):
         db_working.user_id_record(update.message.chat_id)
         bot.send_message(
             chat_id=update.message.chat_id,
-            text='Привет! Назови своё имя'
+            text='Привет! Назови своё имя',
         )
         db_working.set_user_scene_id_now(update.message.chat_id, '00')
+
     else:
         db_working.set_user_scene_id_now(update.message.chat_id, '03')
         bot.send_message(
@@ -59,12 +62,15 @@ def do_any(bot: Bot, update: Update):
         if len(work_cell.links) > 1:
             try:
                 db_working.set_user_scene_id_now(update.message.chat_id, work_cell.links[int(update.message.text) - 1])
-                bot.send_message(
-                    chat_id=update.message.chat_id,
-                    text=db_working.get_cell_text(db_working.get_now_scene_id(update.message.chat_id), update.message.chat_id),
-                    reply_markup=db_working.get_custom_numeric_keyboard(db_working.get_now_scene_id(update.message.chat_id))
-                )
                 work_cell = Cell(db_working.get_now_scene_id(update.message.chat_id))
+                if not len(work_cell.links) < 2:
+                    bot.send_message(
+                        chat_id=update.message.chat_id,
+                        text=db_working.get_cell_text(work_cell.id, update.message.chat_id),
+                        reply_markup=db_working.get_custom_numeric_keyboard(
+                            db_working.get_now_scene_id(update.message.chat_id))
+                    )
+
             except:
                 bot.send_message(
                     chat_id=update.message.chat_id,
@@ -73,18 +79,58 @@ def do_any(bot: Bot, update: Update):
         while len(work_cell.links) == 1:
             if work_cell.type == 'good':
                 db_working.update_user_winscene(update.message.chat_id, work_cell.id[:2])
+                temp_photo = open(r'media_data/win.jpg', 'rb')
+                bot.send_photo(
+                    chat_id=update.message.chat_id,
+                    photo=temp_photo
+                )
+            if work_cell.type == 'bad':
+                temp_photo = open(r'media_data/wasted.jpg', 'rb')
+                bot.send_photo(
+                    chat_id=update.message.chat_id,
+                    photo=temp_photo
+                )
+                temp_photo.close()
             db_working.set_user_scene_id_now(update.message.chat_id, work_cell.links[0])
             bot.send_message(
                 chat_id=update.message.chat_id,
                 text=db_working.get_cell_text(work_cell.links[0], update.message.chat_id),
                 reply_markup=db_working.get_custom_numeric_keyboard(work_cell.links[0])
             )
+
             work_cell = Cell(work_cell.links[0])
     else:
         bot.send_message(
             chat_id=update.message.chat_id,
             text='Неизвестный тип ячейки'
         )
+
+
+def do_check(bot: Bot, update: Update):
+    if str(update.message.chat_id) == '818351859':
+        con = sqlite3.connect('main_data_base.db')
+        cur = con.cursor()
+        users = cur.execute("SELECT * FROM users_id").fetchall()
+        cur.close()
+        con.commit()
+        con.close()
+        for user in users:
+            message = ''
+            if user[3] != None:
+                for branch in re.split(' ', user[3]):
+                    print(branch)
+                    con = sqlite3.connect('main_data_base.db')
+                    cur = con.cursor()
+                    message = message + '- ' + \
+                              cur.execute("SELECT text FROM cells_id WHERE cell_id='{0}'".format(branch)).fetchall()[0][
+                                  0] + '\n'
+                    cur.close()
+                    con.commit()
+                    con.close()
+            bot.send_message(
+                chat_id=update.message.chat_id,
+                text='{0} {1}:\n{2}'.format(user[1], user[2], message)
+            )
 
 
 def main():
@@ -98,11 +144,13 @@ def main():
 
     test_handler = CommandHandler('test', do_test)
     start_handler = CommandHandler('start', do_start)
+    check_handler = CommandHandler('check', do_check)
     message_handler = MessageHandler(Filters.text, do_any)
 
     updater.dispatcher.add_handler(test_handler)
     updater.dispatcher.add_handler(message_handler)
     updater.dispatcher.add_handler(start_handler)
+    updater.dispatcher.add_handler(check_handler)
 
     updater.start_polling()
     updater.idle()
